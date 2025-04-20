f0c5e663fcb1db36ae292b1b1534746813da8b0a
jas7920047 <mambamentality824792004@gmail.com>
2025-04-20 15:51:08 +0530

    Add AndroidAppProject/app/src/main/java/com/example/tabbedapp/MainActivity.java 1745144468509

---
+++ AndroidAppProject/app/src/main/java/com/example/tabbedapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.tabbedapp;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private ViewPagerAdapter adapter;
+
+    private final String[] tabTitles = new String[]{"Blogs", "Forums", "Grocery Items"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+            new TabLayoutMediator.TabConfigurationStrategy() {
+                @Override
+                public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                    tab.setText(tabTitles[position]);
+                }
+            }).attach();
+    }
+}
